64e44546730c3d63e5f391ad4b5186dce1179de5
EirikAsplem <eirikasp@stud.ntnu.no>
2017-03-20 19:55:09 +0100

    Some upgrades to chat. colors, username. Enter now sends message and clears input

--- src/components/chat.js
+++ src/components/chat.js
@@ -2,34 +2,49 @@ import React, { Component } from 'react'
 const io = require('socket.io-client')
 const socket = io()
 
+import styles from './chat.css';
+
 class Chat extends Component {
 
   constructor() {
     super()
     this.state = {
+      userName: "User",
       messages: [],
       team: false,
       input: ""
     }
-    console.log(this.state)
   }
 
   componentDidMount() {
     var that = this
+
     socket.on('message', function(msg){
       var temp = that.state.messages
       temp.push(msg)
       that.setState({messages: temp})
-      console.log(that.state);
+    })
+
+    document.getElementById("message-input").addEventListener("keyup", function(event) {
+      event.preventDefault()
+      if (event.keyCode == 13) {
+        that.sendMessage()
+      }
     })
   }
 
+
+  sendMessage() {
+    socket.emit('message', {msg: this.state.input, userName: this.state.userName})
+    this.refs.messageInput.value = ""
+  }
+
   buttonHandler(event) {
-    console.log(this.state);
-    socket.emit('message', {msg: this.state.input})
+    this.sendMessage()
   }
 
   inputHandler(event) {
+
     var temp = this.state.input
     temp = event.target.value
     this.setState({input: temp})
@@ -38,16 +53,17 @@ class Chat extends Component {
   render() {
     var rows = []
     for (var i = 0; i < this.state.messages.length; i++) {
-      rows.push(<div> {this.state.messages[i].msg} </div>)
+      rows.push(<div className="messages" key={'message' + i} ><div className="message-user-name">{this.state.userName}: </div> <div className="message-text"> {this.state.messages[i].msg} </div> </div>)
     }
     return (
-      <div>
-        Hello
-        {rows}
+      <div className="chat-div">
+        <div className="message-div">
+            {rows}
+        </div>
         <ul id="messages"></ul>
-        <form action="#">
-          <input onChange={this.inputHandler.bind(this)} id="m" /> <button onClick={this.buttonHandler.bind(this)}>Send</button>
-        </form>
+        <div className="input-div" ref="inputDiv">
+            <input onChange={this.inputHandler.bind(this)} ref="messageInput" id="message-input" /> <button onClick={this.buttonHandler.bind(this)}>Send</button>
+        </div>
       </div>
     )
   }
