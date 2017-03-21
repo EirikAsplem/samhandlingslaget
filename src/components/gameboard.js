802cdd19b1d4f7598cabd62a2443f33065e4392e
Sarah Svedenborg <sarah.svedenborg@gmail.com>
2017-03-21 22:20:50 +0100

    fixed slider

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import './gameboard.css'
 import './slider.css'
 import './popup.css'
-//import game from 'img/Froggy1.png'
 
 class Gameboard extends Component {
 
@@ -12,45 +11,6 @@ class Gameboard extends Component {
     popup.classList.toggle("show");
   }
 
-
- /*
-  constructor() {
-    super()
-    this.state = {
-      messages: [],
-      team: false,
-      input: ""
-    }
-    console.log(this.state)
-  }
-
-  componentDidMount() {
-    var that = this
-    socket.on('userConnected', function(msg) {
-      var temp = that.state.messages
-      temp.push(msg)
-      that.setState({messages: temp})
-      console.log(that.state);
-    })
-    socket.on('message', function(msg){
-      var temp = that.state.messages
-      temp.push(msg)
-      that.setState({messages: temp})
-      console.log(that.state);
-    })
-  }
-
-  buttonHandler(event) {
-    console.log(this.state);
-    socket.emit('message', {msg: this.state.input})
-  }
-
-  inputHandler(event) {
-    var temp = this.state.input
-    temp = event.target.value
-    this.setState({input: temp})
-  }
-*/
   render() {
     
     return (
@@ -59,7 +19,7 @@ class Gameboard extends Component {
         <div id="progressBar">
           <span>Opponent</span><input type="range"  min="0" max="100"/> <span>Us</span>
         </div>
-        <img id="gameView" src='http://joshuahunter.com/images/flexbox_froggy_19start.png'/>
+        <img id="gameView" src='/img/Froggy1.png'/>
         <button id="questionMark" onClick={this.togglePopup.bind(this)}>?</button>
         <div className="myPopup"><span id="popuptext">An explanations of the current puzzle will be displayed here. 
         The purpose of this is to provide hints to the players when needed and to understand the solutions of the puzzle at the end.</span></div>
