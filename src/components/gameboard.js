cdcf65785288bd6e2c0506ed66c50da9374a51d9
Sarah Svedenborg <sarah.svedenborg@gmail.com>
2017-03-21 21:29:40 +0100

    added question mark with popup, and 'progressBar' (WIP)

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
+import './gameboard.css'
+import './slider.css'
+import './popup.css'
+//import game from 'img/Froggy1.png'
 
 class Gameboard extends Component {
+
+  togglePopup(event) {
+    console.log("got here");
+    var popup = document.getElementsByClassName("myPopup")[0];
+    popup.classList.toggle("show");
+  }
+
 
  /*
   constructor() {
@@ -44,7 +55,14 @@ class Gameboard extends Component {
     
     return (
       <div id="gameboard">
-        Hello
+        <h1>Crack the Code</h1>
+        <div id="progressBar">
+          <span>Opponent</span><input type="range"  min="0" max="100"/> <span>Us</span>
+        </div>
+        <img id="gameView" src='http://joshuahunter.com/images/flexbox_froggy_19start.png'/>
+        <button id="questionMark" onClick={this.togglePopup.bind(this)}>?</button>
+        <div className="myPopup"><span id="popuptext">An explanations of the current puzzle will be displayed here. 
+        The purpose of this is to provide hints to the players when needed and to understand the solutions of the puzzle at the end.</span></div>
       </div>
     )
   }
